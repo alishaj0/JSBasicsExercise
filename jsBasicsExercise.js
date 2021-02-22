1bbd742ddbbbcc96cc284fea906ee64f364c670f
Alisha Bolin <alishabolin@KCCCs-MBP.lan>
2021-02-22 14:08:02 -0600

    review and completed exercise with all  bonus answers

--- jsBasicsExercise.js
+++ jsBasicsExercise.js
@@ -1,14 +1,14 @@
 // 1) Fav number
     console.log(13);
 
-// 2) Main Data Types:
-    // Numbers, Strings, Booleans, Null, Undefined
+// 2) Main Data Types aka Primitives:
+    // Numbers, Strings, Booleans, Null, Undefined, & Symbols
 
 // 3) 6 Operator Symbols:
     // +  - * / ** %
     // + Add -Subtract *Multiply /Divide **Exponent %Modulus
 
-// 4) Modulus of 1
+// 4) Modulus of 1 (from number 5)
     console.log(5%2);
     console.log(5%4);
 
@@ -19,17 +19,40 @@
     console.log("Wanda" + " " + "Vision");
 
 // 7)
-    console.log("Alisha Bolin".length);
+    console.log("Alisha".length + "Bolin".length);
 
 // 8)
-    console.log("Avengers:Engame"[14]);
+    console.log("Avengers: Endgame"[16]);
 
 
 // BONUS
 // 1) 
     console.log("\\");
+
 // 2)
     console.log((5**4)-9);
+
 // 3)
-    console.log
+    console.log(("I".length % "Am".length)  - ("Iron".length % "Man".length));
+    // console.log(1%2); 
+    // console.log(4%3);
+
 // 4)
+    console.log("CAP"[1]);
+    console.log("VISION"[0]); 
+    console.log("HAWKEYE"[4]);
+    console.log("NICK FURY"[0]); 
+    console.log("GROOT"[0]); 
+    console.log("DOCTOR STRANGE"[13]); 
+    console.log("THOR"[3]); 
+    console.log("STAR LORD"[0]);
+
+    console.log("\n");
+
+    console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "HAWKEYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+
+// 5)
+    var m = 10;
+    var c = 5;
+    var u = m + c; 
+    console.log(u);
