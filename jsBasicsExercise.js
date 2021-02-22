06b18ade71f1951861fa7422f73568825dea9aff
Alisha Bolin <alishabolin@KCCCs-MBP.lan>
2021-02-22 12:19:04 -0600

    completed exercise with first 2 bonus questions

---
+++ jsBasicsExercise.js
@@ -0,0 +1,35 @@
+// 1) Fav number
+    console.log(13);
+
+// 2) Main Data Types:
+    // Numbers, Strings, Booleans, Null, Undefined
+
+// 3) 6 Operator Symbols:
+    // +  - * / ** %
+    // + Add -Subtract *Multiply /Divide **Exponent %Modulus
+
+// 4) Modulus of 1
+    console.log(5%2);
+    console.log(5%4);
+
+// 5)  
+    console.log('I\'M MARRY POPPINS, Y\'ALL!');
+
+// 6) 
+    console.log("Wanda" + " " + "Vision");
+
+// 7)
+    console.log("Alisha Bolin".length);
+
+// 8)
+    console.log("Avengers:Engame"[14]);
+
+
+// BONUS
+// 1) 
+    console.log("\\");
+// 2)
+    console.log((5**4)-9);
+// 3)
+    console.log
+// 4)
